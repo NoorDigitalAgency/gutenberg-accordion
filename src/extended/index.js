1fdf7adefff8489cd2e976e9cdfaee56809cdfbc
Chrillaz <totalchriz@gmail.com>
2020-10-13 11:54:35 +0200

    more icon options implemented

--- src/extended/index.js
+++ src/extended/index.js
@@ -1,6 +1,8 @@
 import { addFilter } from '@wordpress/hooks'
-import { addInlineIconAttribute, withIcon} from './core-button/core-button'
+import { addInlineIconAttribute, withIcon, addExtraProps } from './core-button/core-button'
 
 addFilter( 'blocks.registerBlockType', 'noor/gutenberg-blocks/custom-attributes', addInlineIconAttribute );
 
 addFilter( 'editor.BlockEdit', 'noor/gutenberg-blocks/custom-control', withIcon );
+
+// addFilter( 'blocks.getSaveContent.extraProps', 'noor/gutenberg-blocks/custom-props', addExtraProps );
