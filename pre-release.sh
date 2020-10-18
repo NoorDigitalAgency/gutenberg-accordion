60f2410374a45d99e5b2337e3c9dd35696e1ed4f
Chrillaz <totalchriz@gmail.com>
2020-10-18 22:04:37 +0200

    Latest release v1.0.2

--- pre-release.sh
+++ pre-release.sh
@@ -16,12 +16,12 @@ VERSION=$(grep -w "Version:" $GITPATH/$MAINFILE | awk '{print $3}')
 
 git add .
 
-git commit -m"Latest release $VERSION"
+git commit -m"Latest release v$VERSION"
 
-git tag $VERSION
+git tag v$VERSION
 
 echo
-echo "Pre version: $VERSION"
+echo "Pre version: v$VERSION"
 echo
 
 #sed -i -e "s/\(Version: \).*/\1$TAG/" $GITPATH/$MAINFILE
