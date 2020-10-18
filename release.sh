aa32126c2fe8c19b5a0cedf8bcff4a5495d69a8e
Chrillaz <totalchriz@gmail.com>
2020-10-18 22:46:00 +0200

    removed block copy

---
+++ release.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+MESSAGE="$1"
+
+STABLE="$2"
+
+CURRENTDIR=`pwd`
+
+MAINFILE="blocks.php"
+
+GITPATH="$CURRENTDIR"
+
+echo 
+echo "Preparing for new version."
+echo 
+
+STABLEVERSION=$(grep -w "Stable tag:" $GITPATH/readme.txt | awk '{print $3}')
+
+echo "readme.txt stable version: $STABLEVERSION"
+
+VERSION=$(grep -w "Version:" $GITPATH/$MAINFILE | awk '{print $3}')
+
+echo
+echo "$MAINFILE Version: $VERSION"
+echo
+
+if [ "$STABLEVERSION" != "$VERSION" ]; then echo "Version in readme.txt & $MAINFILE don't match. Exiting...."; exit 1; fi
+
+if git show-ref --tags --quiet --verify -- "refs/tags/$VERSION"
+  then
+    echo "$VERSION already exists as git tag..";
+    exit 1;
+  else echo "Run git pipe."
+fi
+
+cd $GITPATH
+
+echo
+echo "Building..."
+echo
+
+npm run build
+
+echo
+echo "-----------------------------------------------"
+echo
+echo -e "Enter a commit message for this version: \c"
+
+read COMMITMSG
+
+git commit -am"$COMMITMSG"
+
+echo
+echo "Tagging new version $VERSION in git"
+echo
+
+git tag v$VERSION
+
+echo
+echo "Pushing latest commit to origin with tags"
+
+git push origin master
+
+git push origin master --tags
+
+
+echo
+echo "Release: v$VERSION published"
+echo
+
+#sed -i -e "s/\(Version: \).*/\1$TAG/" $GITPATH/$MAINFILE
