47910e5fe33d139098785175e64f959a38d91ab3
Chrillaz <totalchriz@gmail.com>
2020-10-29 16:55:03 +0100

    improved on global show on mobile control

--- src/components/controls.js
+++ src/components/controls.js
@@ -1,17 +1,112 @@
 import { __ } from '@wordpress/i18n'
 import { PanelBody, ToggleControl } from '@wordpress/components'
+import { PanelColorSettings } from '@wordpress/block-editor'
+import { RadioGroup } from '../components/radio-group'
 
-export function SmallScreenControl ( props ) {
+export const SmallScreenControl = ({ setAttributes, attributes: { mediaControl }}) => (
+  <PanelBody
+    title={ __( 'Mobile View Control' ) }
+    initialOpen={ true }
+  > 
+    <ToggleControl
+      label={__(`${mediaControl ? 'Display' : 'Hide'} on mobile`)}
+      checked={mediaControl}
+      onChange={mediaControl => setAttributes({ mediaControl })}
+    />
+  </PanelBody>);
+
+export const ColumnBackgrounControl = ({ backgroundColor, setBackgroundColor, setAttributes }) => {
+
+  setAttributes({ className: backgroundColor.class != undefined ? backgroundColor.class + ' has-background' : '' });
+
+  return (
+    <PanelColorSettings
+      title={__('Color settings')}
+      initialOpen={false}
+      colorSettings={[
+        {
+          value: backgroundColor.color,
+          onChange: setBackgroundColor,
+          label: __('Column background color')
+        }
+      ]}
+    />
+  );
+}
+
+export const ButtonIconControl = ({ attributes: {useIcon, alignIcon, icon}, setAttributes }) => {
+
+  const iconControlOptions = [
+    'arrow-right-alt',
+    'arrow-left-alt',
+    'arrow-right-alt2',
+    'arrow-left-alt2',
+    'clock',
+    'location'
+  ];
+
+  const setIconClassList = () => {
+
+    const align = alignIcon ? 'has-left-icon' : 'has-right-icon';
+
+    const iconName = useIcon && icon !== '' ? `has-icon-${icon}` : '';
+
+    const className = align + ' ' + iconName;
+
+    setAttributes({ className });
+  }
+    
+  const setUseIcon = useIcon => {
+
+    setAttributes({ useIcon });
+
+    if ( ! useIcon ) {
+
+      setIcon( '' );
+    }
+  }
+
+  const setIcon = (value, event) => {
+
+    if ( event ) {
+
+      event.preventDefault();
+    }
+
+    setAttributes({ icon: value });
+
+    setIconClassList();
+  }
+
+  setIcon( icon );
+
   return (
     <PanelBody
-      title={ __( 'Mobile View Control' ) }
+      title={ __( 'Inline Icon Control' ) }
       initialOpen={ true }
     > 
       <ToggleControl
-        label={__(`${props.attributes.mediaControl ? 'Display' : 'Hide'} on mobile`)}
-        checked={props.attributes.mediaControl}
-        onChange={mediaControl => props.setAttributes({ mediaControl })}
+        label={__('Use inline icon')}
+        checked={useIcon}
+        onChange={setUseIcon}
       />
+
+      <ToggleControl 
+        label={alignIcon ? __('Icon before') : __('Icon after')}
+        checked={alignIcon}
+        onChange={() => setAttributes({ alignIcon: ! alignIcon })}
+      />
+
+      {useIcon && <RadioGroup
+        label={__('Inline Icon')} 
+        onClick={(event) => {
+          event.preventDefault();
+          setAttributes({ icon: event.currentTarget.value, event });
+        }}
+        options={iconControlOptions} 
+        initialChecked={icon} 
+        showIcons={true} 
+      />}
     </PanelBody>
   );
 }
--- src/filters/blockEdit.js
+++ src/filters/blockEdit.js
@@ -1,20 +1,14 @@
 import { __ } from '@wordpress/i18n'
 import { createHigherOrderComponent } from '@wordpress/compose'
 import { Fragment } from '@wordpress/element'
-import { withColors, InspectorControls, PanelColorSettings } from '@wordpress/block-editor'
+import { withColors, InspectorControls } from '@wordpress/block-editor'
 import { compose } from '@wordpress/compose'
-import { PanelBody, ToggleControl } from '@wordpress/components'
-import { RadioGroup } from '../components/radio-group'
-import { SmallScreenControl } from '../components/controls'
 
-const iconControlOptions = [
-  'arrow-right-alt',
-  'arrow-left-alt',
-  'arrow-right-alt2',
-  'arrow-left-alt2',
-  'clock',
-  'location'
-];
+import { 
+  SmallScreenControl,
+  ColumnBackgrounControl,
+  ButtonIconControl
+} from '../components/controls'
 
 export const blockEdit = compose(
   withColors({ backgroundColor: 'background-color' }),
@@ -27,114 +21,37 @@ export const blockEdit = compose(
         <BlockEdit {...props} />
         <InspectorControls>
           <SmallScreenControl {...props} />
+          {props.name === 'core/column' && <ColumnBackgrounControl {...props} />}
+          {props.name === 'core/button' && <ButtonIconControl {...props} />}
         </InspectorControls>
       </Fragment>
     )
   }
 
-  if ( props.name === 'core/column' ) {
+  // if ( props.name === 'core/column' ) {
 
-    const {backgroundColor, setBackgroundColor} = props;
+  //   return (
+  //     <Fragment>
+  //       <BlockEdit {...props} />
+  //       <InspectorControls>
+  //         <ColumnBackgrounControl {...props} />
+  //       </InspectorControls>
+  //     </Fragment>
+  //   );
+  // }
 
-    props.setAttributes({ className: backgroundColor.class != undefined ? backgroundColor.class + ' has-background' : '' });
-
-    return (
-      <Fragment>
-        <BlockEdit {...props} />
-        <InspectorControls>
-          <PanelColorSettings
-            title={__('Color settings')}
-            initialOpen={false}
-            colorSettings={[
-              {
-                value: backgroundColor.color,
-                onChange: setBackgroundColor,
-                label: __('Column background color')
-              }
-            ]}
-          />
-        </InspectorControls>
-      </Fragment>
-    );
-  }
-
-  if ( props.name === 'core/button' ) {
-
-    const {attributes, setAttributes} = props;
-
-    const {useIcon, alignIcon, icon} = props.attributes;
+  // if ( props.name === 'core/button' ) {
     
-    const setIconClassList = () => {
+  //   return (
+  //     <Fragment>  
+  //       <BlockEdit { ...props } />
 
-      const align = alignIcon ? 'has-left-icon' : 'has-right-icon';
-
-      const iconName = useIcon && icon !== '' ? `has-icon-${icon}` : '';
-
-      const className = align + ' ' + iconName;
-
-      setAttributes({ className });
-    }
-    
-    const setUseIcon = useIcon => {
-
-      setAttributes({ useIcon });
-
-      if ( ! useIcon ) {
-
-        setIcon( '' );
-      }
-    }
-
-    const setIcon = (value, event) => {
-
-      if ( event ) {
-
-        event.preventDefault();
-      }
-
-      setAttributes({ icon: value });
-
-      setIconClassList();
-    }
-
-    setIcon( icon );
-    
-    return (
-      <Fragment>  
-        <BlockEdit { ...props } />
-
-        <InspectorControls>
-          <PanelBody
-            title={ __( 'Inline Icon Control' ) }
-            initialOpen={ true }
-          > 
-            <ToggleControl
-              label={__('Use inline icon')}
-              checked={useIcon}
-              onChange={setUseIcon}
-            />
-
-            <ToggleControl 
-              label={alignIcon ? __('Icon before') : __('Icon after')}
-              checked={alignIcon}
-              onChange={() => setAttributes({ alignIcon: ! alignIcon })}
-            />
-
-            {useIcon && <RadioGroup
-              label={__('Inline Icon')} 
-              onClick={(event) => {
-                event.preventDefault();
-                setAttributes({ icon: event.currentTarget.value, event });
-              }}
-              options={iconControlOptions} 
-              initialChecked={props.attributes.icon} 
-              showIcons={true} 
-            />}
-          </PanelBody>
-        </InspectorControls>
-      </Fragment>
-    );
-  }
+  //       <InspectorControls>
+  //         <ButtonIconControl {...props} />
+  //       </InspectorControls>
+  //     </Fragment>
+  //   );
+  // }
 
   return <BlockEdit {...props} />
 }, 'blockEdit'));
